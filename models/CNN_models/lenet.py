1dbf3c089a51aa2d88ccd5f44864cc24bce1e97e
Seangottarun <sean.wu20s@outlook.com>
2020-10-17 19:08:08 -0400

    Convert LeNet to Sequential model instead of Keras subclassing

--- models/CNN_models/lenet.py
+++ models/CNN_models/lenet.py
@@ -1,45 +1,18 @@
 import tensorflow as tf
+from tensorflow.keras import layers
 
-class LeNet(tf.keras.Model):
-    def __init__(self, **kwargs):
-        super(LeNet, self).__init__()
-        self.conv_layer_1 = tf.keras.layers.Conv2D(
-                filters=6,
-                kernel_size=(5, 5),
-                input_shape=(28, 28, 1),
-                padding='valid',
-                activation=tf.nn.relu
-                )
-        self.pool_layer_1 = tf.keras.layers.MaxPooling2D(padding='same')
-        self.conv_layer_2 = tf.keras.layers.Conv2D(
-                filters=16,
-                kernel_size=(5, 5),
-                padding='valid',
-                activation=tf.nn.relu
-                )
-        self.pool_layer_2 = tf.keras.layers.MaxPooling2D(padding='same')
-        self.flatten = tf.keras.layers.Flatten()
-        self.fc_layer_1 = tf.keras.layers.Dense(
-                units=120,
-                activation=tf.nn.relu
-                )
-        self.fc_layer_2 = tf.keras.layers.Dense(
-                units=84,
-                activation=tf.nn.relu
-                )
-        self.output_layer = tf.keras.layers.Dense(
-                units=kwargs['num_classes'],
-                activation=tf.nn.softmax
-                )
+def build_LeNet(input_shape=(240,240,3)):
+    layer_list=[layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu', input_shape=input_shape),#(240,240,3)),#input_shape=(32,32,1)),
+                layers.AveragePooling2D(),
+                layers.BatchNormalization(),
+                layers.Conv2D(filters=6, kernel_size=(3, 3), activation='relu'),
+                layers.AveragePooling2D(),
+                layers.BatchNormalization(),
+                layers.Flatten(),
+                layers.Dense(units=96, activation='relu'),
+                layers.Dense(units=84, activation='relu'),
+                layers.Dense(units=32, activation = 'softmax')
+                ]
+    model = tf.keras.Sequential(layer_list, name="LeNet")
 
-    @tf.function
-    def call(self, features):
-        activation = self.conv_layer_1(features)
-        activation = self.pool_layer_1(activation)
-        activation = self.conv_layer_2(activation)
-        activation = self.pool_layer_2(activation)
-        activation = self.flatten(activation)
-        activation = self.fc_layer_1(activation)
-        activation = self.fc_layer_2(activation)
-        output = self.output_layer(activation)
-        return output
+    return model

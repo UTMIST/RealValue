4a21ceaa6c6a5fcf5395490db8c259e6c3ca3042
Seangottarun <sean.wu20s@outlook.com>
2020-10-17 19:10:26 -0400

    Fix get_network to work with sequential LeNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,30 +1,35 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-#from models.CNN_models.vgg import VGG16
-#from models.CNN_models.ResNet18 import ResNet18
-from models.CNN_models.lenet import LeNet
-#from models.CNN_models.name_of_file_import name_of_class
 
-def get_network(CNN_name, dense_layers) -> None:
-    #create dense network dynamically based on input 
-    dense_model = keras.Sequential(
-        [
-            for i in range(len(dense_layers)-1): #length of dense_layers is how many layers there will be
-                name = "layer" + str(i+1)
-                layers.Dense(dense_layers[i], activation="relu", name=name),
-            name = "layer" + str(len(dense_layers))
-            layers.Dense(dense_layers[len(dense_layers)-1], name=name), #final output layer, no activation for next layer
-        ]
-    )
+from models.CNN_models.lenet import build_LeNet
+from models.CNN_models.MiniVGGNet import MiniVGGNetModel
+
+def get_network(CNN_name, dense_layers, CNN_input_shape) -> None:
+    #create dense network dynamically based on input
+    layer_list=[keras.Input(shape=(24,))]
+    for i in range(len(dense_layers)):
+        name = "layer" + str(i+1)
+        layer_list+=[layers.Dense(dense_layers[i], activation="relu", name=name)] #final output layer, no activation for next layer
+
+    dense_model = keras.Sequential(layer_list)
+    print(dense_model.summary())
     #select the CNN network
-    if name == 'LeNet':
-        CNN_model = LeNet()
-    elif name == 'VGG16':
+
+    if CNN_name == 'LeNet':
+        CNN_model = build_LeNet(input_shape=CNN_input_shape)
+    elif CNN_name == 'MiniVGG':
+        CNN_model = MiniVGGNetModel()
+    elif CNN_name == 'VGG16':
         CNN_model = VGG16()
-    elif name == 'ResNet':
+    elif CNN_name == 'ResNet':
         CNN_model = ResNet18()
     else:
         CNN_model = None
+    print('True')
 
+    print(CNN_model.summary())
     return dense_model, CNN_model
+
+# get_network('LeNet',[5,4,4])
+#get_network('MiniVGG',[5,4,4])

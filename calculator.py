6862283a4a4bc2cad339336a8d888cf36777b323
arshinmar <arsh.kadakia18@gmail.com>
2021-01-31 16:45:01 -0500

    new calculator

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ f = open("temp.txt", "r")
 counter=0
 
 prev=f.readline()
-temp=[]
-
+num1,num2,sum=0,0,0
+while (prev!=""):
+    if (prev[0:3]=="Dim" and prev[3]!=' '):
+        numbers = prev[3:]
+        num1=0
+        num2=0
+        for i in numbers.split():
+            try:
+                float(i)
+                a=True
+            except:
+                a=False
+            if a==True:
+                if num1==0:
+                    num1=float(i)
+                elif num2==0:
+                    num2=float(i)
+        print(num1,"NUM1")
+        print(num2,"NUM2")
+        sum=sum+num1*num2
+    prev = f.readline()
+print('__________________________________')
+print(sum, "= Square Metres (Assuming temp.txt has metric units)")
+print(sum*10.764, "= IMPERIAL Square Feet (Assuming temp.txt has metric units)")
+'''temp=[]
 
 num1=0
 num2=0
@@ -37,7 +60,4 @@ while (prev!=""):
     prev=f.readline()
     counter+=1
     print("")
-print(temp)
-print('__________________________________')
-print(sum, "= Square Metres (Assuming temp.txt has metric units)")
-print(sum*10.764, "= IMPERIAL Square Feet (Assuming temp.txt has metric units)")
+'''

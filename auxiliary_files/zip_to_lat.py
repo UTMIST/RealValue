806d26bbed67fd11ab0cacc13cb514b857ee2a36
arshinmar <arsh.kadakia18@gmail.com>
2021-03-13 00:11:43 -0500

    Updated with 18% test error

--- auxiliary_files/zip_to_lat.py
+++ auxiliary_files/zip_to_lat.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+'''
 toronto_dataset = pd.read_excel('TorontoDataset.xlsx')
 toronto_dataset_2 = toronto_dataset.copy()
 toronto_dataset = toronto_dataset.loc[:,["Zip Code"]]
@@ -29,4 +29,45 @@ toronto_dataset_2 = toronto_dataset_2.drop_duplicates()
 result = pd.DataFrame(toronto_dataset_2.reset_index(drop=True))
 
 result = result.drop(columns = ["Zip Code"])
-result.to_excel('TorontoDataset_new.xlsx')
+result.to_excel('TorontoDataset_new.xlsx')'''
+
+
+
+#df = pd.read_csv('../USZIPCodes202103.csv', delimiter = ",")
+
+#temp_try = pd.concat([main_dataframe, main_txt], axis=1, join="inner")
+
+df = pd.read_csv('../USZIPCodes202103.csv', delimiter = ",")
+main_dataframe = df.loc[:,["Zip Code","ZipLatitude","ZipLongitude"]]
+main_dataframe = main_dataframe.rename(columns={"Zip Code": "Zip Code", "ZipLatitude": "Lat", "ZipLongitude":"Long"})
+
+main_txt = pd.read_csv('../cali_toronto_raw_dataset/HousesInfo.txt', delimiter = " ",names=['Bedrooms','Bathrooms','SqFt','Zip Code','Price'])
+
+main_txt = main_txt.reset_index().rename({'index':'index1'}, axis = 'columns')
+print(main_dataframe)
+print(main_txt)
+
+temp_try = main_txt.join(main_dataframe.set_index('Zip Code'), on = 'Zip Code')
+print(temp_try)
+temp_try = temp_try.drop_duplicates()
+temp_try = temp_try[['Bedrooms','Bathrooms','SqFt','Price','Lat','Long']]
+
+long_max, long_min = temp_try['Long'].max(), temp_try['Long'].min()
+lat_max, lat_min = temp_try['Lat'].max(), temp_try['Lat'].min()
+
+
+temp_try['Long'] = (temp_try['Long'] - long_min) / (long_max - long_min)
+
+temp_try['Lat'] = (temp_try['Lat'] - lat_min) / (lat_max - lat_min)
+
+
+
+temp_try.to_excel('../raw_dataset_true.xlsx')
+
+blah = temp_try['Lat'].isnull()
+print('---------')
+print(blah)
+print(blah.sum())
+print('---------')
+
+print(temp_try)
